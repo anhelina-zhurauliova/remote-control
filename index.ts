03e5b96808c08c50b53eaa2366d2b4f55bad6686
Anhelina Zhurauliova <anhelinazhurauliova@gmail.com>
2022-06-26 18:22:00 +0200

    feat: handle program exit

--- index.ts
+++ index.ts
@@ -13,3 +13,18 @@ const ws: WebSocketServer = new WebSocketServer({
 });
 
 ws.on("connection", handleWS);
+
+const handleExit = () => {
+  httpServer.close(() => {
+    process.stdout.write(
+      `Static http server on the ${HTTP_PORT} port was closed \n`
+    );
+  });
+
+  ws.close(() => {
+    process.stdout.write(`Web socket on the port ${WS_PORT} was closed \n`);
+  });
+};
+
+process.on("exit", handleExit);
+process.on("SIGINT", handleExit);
